1069f3ebb052363d5a35082e77b89091cde91641
ComFel <felipevm07@gmail.com>
2023-01-17 20:22:13 +0100

    Demo project

---
+++ DemoEngine/code/source/WinMain.cpp
@@ -0,0 +1,13 @@
+/// @author Ángel
+
+#define WIN32_LEAN_AND_MEAN
+#define VC_EXTRALEAN
+
+#include <Windows.h>
+
+int main ();
+
+int WINAPI WinMain (HINSTANCE, HINSTANCE, LPSTR, INT)
+{
+	return main ();
+}
